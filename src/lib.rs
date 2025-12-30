ec783f1b9921829f1ca82277c43eec2eac5f0588
Pierre Mavro <deimos@deimos.fr>
2025-12-30 18:22:37 +0100

    feat: update libs and cargo version

--- src/lib.rs
+++ src/lib.rs
@@ -110,7 +110,7 @@ pub fn perform_commit(repo_path: &Path, full_commit_message: &str) -> Result<(),
         Some("HEAD"),
         &sig,
         &sig,
-        &full_commit_message,
+        full_commit_message,
         &tree,
         &parents,
     )?;
